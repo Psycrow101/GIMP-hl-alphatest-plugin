54c527f9c4b3645dafac0ec9e379ce487ecb5d6c
Psycrow <psycrow101@gmail.com>
2020-09-25 22:33:58 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Half-Life Alphatest for GIMP
+The plugin converts the image to color indexing mode, moving the alpha channel colors as the last index in the palette. This allows to quickly prepare bitmap images as textures in alphatest mode for Half-Life game.
+
+## Requirements
+1. [GIMP](https://www.gimp.org/), recommended GIMP version >= 2.10.  
+2. GIMP's python module gimpfu.  
+
+## Installation
+Download and extract the `hl-alphatest` folder to GIMP's `plug-ins` folder:  
+	**Windows**: `C:\Users\<USERNAME>\AppData\Roaming\GIMP\2.10\plug-ins`  
+	**Linux**: `/home/<USERNAME>/.config/GIMP/2.10/plug-ins`  
+	**macOS**: `/Users/<USERNAME>/Library/Application Support/GIMP/2.10/plug-ins`
+
+*If you can’t locate the `plug-ins` folder, open GIMP and go to Edit > Preferences > Folders > Plug-Ins and use one of the listed folders.*
+
+## Usage
+Go to Image > Half-Life > Alphatest and select the maximum alpha channel value for filtering and the dither type. This menu will be available only for images with transparent colors. If your image is already in indexed mode, switch to RGB mode (Image > Mode > RGB).
+
+*After using the plugin, your layer groups will be merged.*
---
+++ hl-alphatest/hl-alphatest.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python2
+
+from gimpfu import *
+
+
+AUTHOR           = 'Psycrow'
+COPYRIGHT        = AUTHOR
+COPYRIGHT_YEAR   = '2020'
+
+LOAD_PROC        = 'hl-alphatest'
+
+
+def hl_alphatest(image, drawable, power, dither_type):
+    pdb.gimp_context_push()
+    pdb.gimp_image_undo_group_start(image)
+
+    # Gimp does not support indexed mode if the image contains layer groups, so delete them
+    for layer in image.layers:
+        if pdb.gimp_item_is_group(layer):
+            pdb.gimp_image_merge_layer_group(image, layer)
+    pdb.gimp_image_convert_indexed(image, dither_type, MAKE_PALETTE, 255, 0, 0, '')
+
+    num_bytes, colormap = pdb.gimp_image_get_colormap(image)
+    pdb.gimp_image_set_colormap(image, num_bytes + 3, list(colormap) + [0, 0, 255])
+
+    last_index = chr(num_bytes // 3)
+    layers_num = len(image.layers)
+
+    gimp.progress_init('Converting %d %s to alphatest' % (layers_num, 'layer' if layers_num == 1 else 'layers'))
+
+    for l, layer in enumerate(image.layers):
+        if layer.type == INDEXED_IMAGE:
+            continue
+
+        pr = layer.get_pixel_rgn(0, 0, layer.width, layer.height)
+        indices = pr[:, :]
+        new_indices = ''
+        for i in xrange(0, len(indices), 2):
+            idx, alpha = indices[i:i+2]
+            new_indices += last_index + '\x00' if ord(alpha) <= power else idx + '\xFF'
+        pr[:, :] = new_indices
+        layer.flush()
+
+        gimp.progress_update(l / float(layers_num))
+
+    pdb.gimp_displays_flush()
+
+    pdb.gimp_image_undo_group_end(image)
+    pdb.gimp_context_pop()
+
+
+register(
+    LOAD_PROC,
+    'Converts the image to color indexing mode, moving the alpha channel colors as the last index in the palette',
+    '',
+    AUTHOR,
+    COPYRIGHT,
+    COPYRIGHT_YEAR,
+    'Alphatest',
+    'RGBA, GRAYA',
+    [
+        (PF_IMAGE, 'image', 'Input image', None),
+        (PF_DRAWABLE, 'drawable', 'Input drawable', None),
+        (PF_SLIDER, 'power', 'Maximum alpha value {0 - 255}', 0, (0, 255, 1)),
+        (PF_OPTION, 'dither-type', 'The dither type to use', 0, (
+            'None',
+            'FS (normal)',
+            'FS (reduced color bleeding)',
+            'Positioned'
+        )),
+    ],
+    [],
+    hl_alphatest, menu='<Image>/Image/Half-Life/'
+)
+
+main()
